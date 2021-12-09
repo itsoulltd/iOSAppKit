03b17a81f7474dfb70463f187ba8b1abb7cea744
Towhid Islam <m.towhid.islam@gmail.com>
2021-12-09 21:31:38 +0600

    continue...with working startup code.

--- iOSAppKit/iOSAppKitExample/CustomRouter/CustomTransitioningAnimator.swift
+++ iOSAppKit/iOSAppKitExample/CustomRouter/CustomTransitioningAnimator.swift
@@ -24,12 +24,11 @@ class CustomTransitioningAnimator: NSObject, UIViewControllerAnimatedTransitioni
         let finalFrameForVC = transitionContext.finalFrame(for: toViewController)
         let containerView = transitionContext.containerView
         let bounds = UIScreen.main.bounds
-        
+        //
         if reverse == false{
-            
             toViewController.view.frame = finalFrameForVC.offsetBy(dx: 0, dy: -bounds.size.height)
             containerView.addSubview(toViewController.view)
-            
+            //
             UIView.animate(withDuration: transitionDuration(using: transitionContext), delay: 0.0, usingSpringWithDamping: 0.65, initialSpringVelocity: 0.0, options: UIViewAnimationOptions(), animations: {
                     fromViewController.view.alpha = 0.5
                     toViewController.view.frame = finalFrameForVC
@@ -39,19 +38,16 @@ class CustomTransitioningAnimator: NSObject, UIViewControllerAnimatedTransitioni
             })
         }
         else{
-            
+            //
             toViewController.view.frame = finalFrameForVC
             toViewController.view.alpha = 0.5
-            //containerView.addSubview(toViewController.view)
-            //containerView.sendSubview(toBack: toViewController.view)
-            
+            //
             let snapshot = fromViewController.view.snapshotView(afterScreenUpdates: false)!
             snapshot.frame = fromViewController.view.frame
             containerView.addSubview(snapshot)
             let snapshotFinalFrame = finalFrameForVC.offsetBy(dx: 0, dy: -bounds.size.height)
-            
             fromViewController.view.removeFromSuperview()
-            
+            //
             UIView.animate(withDuration: transitionDuration(using: transitionContext), delay: 0.0, usingSpringWithDamping: 0.65, initialSpringVelocity: 0.0, options: UIViewAnimationOptions(), animations: {
                     snapshot.frame = snapshotFinalFrame
                     toViewController.view.alpha = 1.0
